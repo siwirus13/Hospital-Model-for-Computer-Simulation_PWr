5c9a8a3a3aec354baad3a4860e6afecf6b2b0513
Mateusz Pilecki <147971553+siwirus13@users.noreply.github.com>
2025-01-15 18:38:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from simulation import Simulation
+import logging
+import csv
+from pathlib2 import Path
 
-def main():
-    sim = Simulation(seed=80, patient_prob=0.99, endtime=500)
+def main2():
+    sim = Simulation(seed=80, patient_prob=0.9, endtime=10)
     sim.run('Triage', beds=40)
-    sim.run('Resource Allocation', red_beds=10, orange_beds=10, yellow_beds=10, green_beds=10)
-
-if __name__ == "__main__":
-    main()
+    # sim.run('Resource Allocation', red_beds=10, orange_beds=10, yellow_beds=10, green_beds=10)
+main2()
