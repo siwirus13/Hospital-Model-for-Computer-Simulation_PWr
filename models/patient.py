19b68edf90e5421d79310bcf3dd1268b3fef9559
Mateusz Pilecki <147971553+siwirus13@users.noreply.github.com>
2025-01-15 18:38:52 +0100

    Update patient.py

--- models/patient.py
+++ models/patient.py
@@ -1,6 +1,8 @@
+import random
+
 class Patient:
     def __init__(self, health_status, waiting_time=0, hospital_time=0):
-        self.health_status = health_status
+        self.health_status = health_status  # 'green', 'yellow', 'orange', 'red'
         self.waiting_time = waiting_time
         self.hospital_time = hospital_time
 
@@ -19,4 +21,26 @@ class Patient:
             self.health_status = 'yellow'
         elif self.health_status == 'yellow':
             self.health_status = 'green'
-##Still in progress
+
+    def state_change(self):
+        base_prob_worsen = {'green': 0.1, 'yellow': 0.2, 'orange': 0.4, 'red': 0.7}
+        base_prob_improve = {'green': 0.3, 'yellow': 0.2, 'orange': 0.15, 'red': 0.1}
+
+        waiting_factor = min(1.0, self.waiting_time / 10)
+        hospital_factor = min(1.0, self.hospital_time / 10)
+
+        prob_worsen = base_prob_worsen[self.health_status] + 0.1 * waiting_factor
+        prob_improve = base_prob_improve[self.health_status] + 0.1 * hospital_factor
+
+        total_prob = prob_worsen + prob_improve
+        prob_worsen /= total_prob
+        prob_improve /= total_prob
+
+        random_value = random.random()
+        if random_value < prob_worsen:
+            self.worsen()
+
+        elif random_value < prob_worsen + prob_improve:
+            self.improve()
+
+
