d91c8055ecb5956156206abc330a6e98ef52c0ea
Mateusz Pilecki <147971553+siwirus13@users.noreply.github.com>
2025-01-21 20:34:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 from simulation import Simulation
-import logging
-import csv
-from pathlib2 import Path
+from patient_generation import PatientGenerator
 
-def main2():
-    sim = Simulation(seed=80, patient_prob=0.9, endtime=10)
-    sim.run('Triage', beds=40)
-    # sim.run('Resource Allocation', red_beds=10, orange_beds=10, yellow_beds=10, green_beds=10)
-main2()
+def main():
+    seed = 80
+    patient_prob = 0.9
+    endtime = 3
+    beds = 10
+    beds_colors = [10, 10, 10, 10]
+
+    all_patients=PatientGenerator(seed, patient_prob, endtime).generate_all_patients()
+    print("\n")
+    sim = Simulation(seed, patient_prob, endtime, all_patients)
+    sim.run('Triage', beds=beds)
+    print("\n")
+    sim.run('Resource Allocation', red_beds=beds_colors[0], orange_beds=beds_colors[1], yellow_beds=beds_colors[2], green_beds=beds_colors[3])
+main()
