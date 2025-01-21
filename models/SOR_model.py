30d91a5f876cb0050954022b52416faae97a9893
Mateusz Pilecki <147971553+siwirus13@users.noreply.github.com>
2025-01-21 20:35:21 +0100

    Update SOR_model.py

--- models/SOR_model.py
+++ models/SOR_model.py
@@ -1,71 +1,18 @@
 from abc import ABC, abstractmethod
-import random
-from models.patient import Patient
 
 class SORModel(ABC):
-    def __init__(self, endtime, patient_prob):
+    def __init__(self, endtime, patient_prob, hourly_patients=None):
         self.endtime = endtime
         self.patient_prob = patient_prob
-        self.hospitalized = []
         self.waiting = []
+        self.hospitalized = []
         self.recoveries = 0
         self.deaths = 0
         self.resources_used = 0
-        self.generated_patients = []
+        self.hourly_patients = hourly_patients or {}
 
-
-
-    def generate_patients(self, amount):
-        new_patients = []
-        for time in range(amount):
-            if random.random() < self.patient_prob:
-                health_status = random.choices(
-                    ['green', 'yellow', 'orange', 'red'],
-                    weights=[0.35, 0.25, 0.25, 0.15],
-                    k=1
-                )[0]
-                new_patients.append(Patient(health_status))
-                print(f"New patient with health status {health_status} arrived.")
-            else:
-                break
-        return new_patients
-
-    def ambulanse(self,patient_prob, endtime):
-
-        new_patients = 0
-        for time in range(endtime):
-
-            while True:
-                if random.random() < patient_prob:
-                    new_patients += 1
-
-                else:
-                    break
-            return new_patients
-
-
-    def state_change(self):
-        # Aktualizowanie stanu pacjentów oczekujących
-        for patient in self.waiting:
-            patient.state_change()
-            patient.waiting_time += 1
-            if patient.health_status == 'red' and patient.waiting_time >= 10:
-                self.deaths += 1
-                self.waiting.remove(patient)
-                print(f"Patient in waiting died after {patient.waiting_time} hours.")
-
-        # Aktualizowanie stanu pacjentów hospitalizowanych
-        for patient in self.hospitalized:
-            patient.state_change()
-            patient.hospital_time += 1
-            if patient.health_status == 'green':
-                self.recoveries += 1
-                self.resources_used += 2  # Przykładowy koszt dla zielonego pacjenta
-                self.hospitalized.remove(patient)
-            elif patient.health_status == 'red' and patient.hospital_time >= 24:
-                self.deaths += 1
-                self.resources_used += 12  # Przykładowy koszt dla czerwonego pacjenta
-                self.hospitalized.remove(patient)
+    def get_patients_for_hour(self, time):
+        return self.hourly_patients.get(time, [])
 
     @abstractmethod
     def run(self):
