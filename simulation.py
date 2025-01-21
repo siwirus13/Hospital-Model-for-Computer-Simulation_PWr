0013112224ee3d3f9b3ff1bd9c7cc12a2a52e39d
Mateusz Pilecki <147971553+siwirus13@users.noreply.github.com>
2025-01-21 20:34:44 +0100

    Update simulation.py

--- simulation.py
+++ simulation.py
@@ -1,19 +1,22 @@
 from models.triage_model import TriageModel
 from models.resource_allocation_model import ResourceAllocationModel
-import random
+
+
 
 class Simulation:
-    def __init__(self, seed, patient_prob, endtime):
-        random.seed(seed)
+    def __init__(self, seed, patient_prob, endtime, hourly_patients):
+        self.seed = seed
         self.patient_prob = patient_prob
         self.endtime = endtime
+        self.hourly_patients = hourly_patients
+        self.waiting = []
+        self.hospitalized = []
 
     def run(self, model_type, **kwargs):
         if model_type == 'Triage':
-            model = TriageModel(*kwargs.values(), self.endtime, self.patient_prob)
+            model = TriageModel(self.endtime, self.patient_prob, self.hourly_patients, **kwargs)
         elif model_type == 'Resource Allocation':
-            model = ResourceAllocationModel(*kwargs.values(), self.endtime, self.patient_prob)
+            model = ResourceAllocationModel(self.endtime, self.patient_prob, self.hourly_patients, **kwargs)
         else:
             raise ValueError("Invalid model type.")
         model.run()
-
